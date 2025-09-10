bed28732240d43b9392e16257100d46d54cf4813
Tetiana2904 <tatianaorlenko20942002@gmail.com>
2025-09-10 19:59:11 -0400

    Add files via upload

--- Hillel.py
+++ Hillel.py
@@ -1,8 +1,39 @@
-num = int(input("5-значне число: "))
-a = num // 10000
-b = (num % 10000) // 1000
-c = (num % 1000) // 100
-d = (num % 100) // 10
-e = num % 10
-reversed_num = e*10000 + d*1000 + c*100 + b*10 + a
-print(reversed_num)
+a = float(input("перше число: "))
+b = float(input("друге число: "))
+op = input("Що зробити?(+, -, *, /): ")
+
+if op == "+":
+    print("Результат =", a + b)
+elif op == "-":
+    print("Результат =", a - b)
+elif op == "*":
+    print("Результат =", a * b)
+elif op == "/":
+    if b == 0:
+        print("Не ділиться")
+    else:
+        print("Результат =", a / b)
+
+
+
+def move_last(lst):
+    if len(lst) == 0 or len(lst) == 1:
+        return lst
+    new_list = [lst[-1]]
+    for i in range(len(lst)-1):
+        new_list.appen
+
+
+def split_list(lst):
+    if len(lst) == 0:
+        return [[], []]
+    mid = (len(lst) + 1) // 2
+    first = []
+    second = []
+    for i in range(mid):
+        first.append(lst[i])
+    for i in range(mid, len(lst)):
+        second.append(lst[i])
+    return [first, second]
+
+
