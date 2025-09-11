0bdc5204fceb28ebf1a55ebc0707d9fb0fbf0761
Tetiana2904 <tatianaorlenko20942002@gmail.com>
2025-09-10 20:14:54 -0400

    Update Hillel.py

--- Hillel.py
+++ Hillel.py
@@ -1,10 +1,15 @@
-def sum_even_indexes(lst):
-    if len(lst) == 0:
-        return 0
-    total = 0
-    for i in range(0, len(lst), 2):
-        total += lst[i]
-    return total * lst[-1]
+import random
+
+n = random.randint(4, 8)   
+lst = []
+
+for i in range(n):
+    lst.append(random.randint(10, 99))  
+
+if len(lst) >= 3:
+    new_lst = [lst[0], lst[2], lst[1]]
+else:
+    new_lst = lst
 
 
 
